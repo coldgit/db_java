4a06ecd43bd2a862ec7d6d44cec44fbc434d23ea
Jiyu Win <akosijiyuwin@gmail.com>
2016-10-23 05:44:02 +0800

    ./.

--- pQuest.java
+++ pQuest.java
@@ -13,21 +13,21 @@ class pQuest extends JFrame
 {
 	public static String[] dsn = {"jdbc:mysql://localhost:3306/db_java","root",""};
 
-	public static void Query(String sql)
+	public static Vector Query(String sql)
 	{
+		Vector rows = new Vector();
+		
 		try{
 			Class.forName("com.mysql.jdbc.Driver");
 			Connection con  = DriverManager.getConnection(dsn[0],dsn[1],dsn[2]);
 			Statement stm = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
 			ResultSet rs = stm.executeQuery(sql);
 			ResultSetMetaData rsmd = rs.getMetaData();
-			
-			int col = rsmd.getColumnCount();
 			Vector memories = new Vector();
-			final Vector rows = new Vector();
-			int outer=0;
+			int col = rsmd.getColumnCount();
 			while(rs.next())
 		    {
+		    	
      			for(int inner = 0; inner < col; inner++)
  				{
  					memories.add(rs.getString(inner+1));
@@ -38,15 +38,17 @@ class pQuest extends JFrame
  				System.out.println("Memory DELETED:"+rows.get(0));
 		    }
 			System.out.println("Memories After Being Removed! : "+memories.size());
+			
 		}catch(Exception e)
 		{
 			System.out.println(e);
 		}
+		return rows;
 	}
-
+	
 	public static void main(String[] args)
 	{
-		Query("SELECT user_id,username,password FROM users");
+		System.out.println(Query("SELECT user_id,username,password FROM users"));
 	}
 
 }
